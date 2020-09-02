054c6bed9f73711ff128efd97eaf34e291bc636f
NEO <itsneo1990@gmail.com>
2020-09-02 16:52:03 +0800

    fix: url正则

--- src/codebase/urls.py
+++ src/codebase/urls.py
@@ -11,6 +11,6 @@ HANDLERS = [
     url(r"/auth/hydra/login.form", hydra.DefaultLoginHandler),
     url(r"/auth/hydra/consent.form", hydra.DefaultConsentHandler),
     url(r"/user", user.UserHandler),
-    url(r"/user/(\d+)/reset_password", user.UserResetPasswordHandler),
-    url(r"/user/(\d+)", user.UserDetailHandler),
+    url(r"/user/([\w\-]+)/reset_password", user.UserResetPasswordHandler),
+    url(r"/user/([\w\-]+)", user.UserDetailHandler),
 ]
